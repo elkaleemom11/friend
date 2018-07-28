b8c069fd9a6ba5e891f43054483cadd480ce91ad
Connie <elkaleemom11@gmail.com>
2018-07-28 11:53:33 -0600

    more edits to server.js

--- server.js
+++ server.js
@@ -11,3 +11,15 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
